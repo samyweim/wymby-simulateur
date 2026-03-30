e9f68385e319eb9b2f84f2378a68d5d6f39f46fd
Samy Weimberg <samyweim@gmail.com>
2026-03-30 10:20:07 +0200

    chore: add gitignore and stop tracking generated files

--- packages/engine/dist/log-formatter.d.ts
+++
@@ -1,6 +0,0 @@
-import type { EngineLog, LogLevel } from "./logger.js";
-export declare function formatEngineLogs(logs: EngineLog[]): string;
-export declare function filterLogs(logs: EngineLog[], minLevel: LogLevel): EngineLog[];
-export declare function filterLogsByScenario(logs: EngineLog[], scenario_id: string): EngineLog[];
-export declare function summarizeLogs(logs: EngineLog[]): string;
-//# sourceMappingURL=log-formatter.d.ts.map
